ef7f145e68c9b7d9912acc9fc2134885392a209c
steve <steve.kossouho@gmail.com>
2015-04-06 15:48:12 +0200

    Upgraded the Dashboard and Menu sub-apps to the Django 1.7 app system, to fix migration issues in Django 1.8

--- admin_tools/dashboard/__init__.py
+++ admin_tools/dashboard/__init__.py
@@ -1,2 +1,14 @@
 from admin_tools.dashboard.dashboards import *
 from admin_tools.dashboard.registry import *
+from django.apps.config import AppConfig
+
+
+class AdminToolsDashboardConfig(AppConfig):
+    """ Admin tools dashboard app configuration """
+    name = 'admin_tools_dashboard'
+    label = 'admin_tools.dashboard'
+
+    def ready(self):
+        """ Actions when django's app registry is ready """
+        pass
+
--- admin_tools/menu/__init__.py
+++ admin_tools/menu/__init__.py
@@ -1,1 +1,13 @@
 from admin_tools.menu.menus import *
+from django.apps.config import AppConfig
+
+
+class AdminToolsMenuConfig(AppConfig):
+    """ Admin tools menu app configuration """
+    name = 'admin_tools_menu'
+    label = 'admin_tools.menu'
+
+    def ready(self):
+        """ Actions when django's app registry is ready """
+        pass
+
